59f43436395ada37d3f38420c8bf030285c5b8da
210041258 <142579249+210041258@users.noreply.github.com>
2024-12-22 01:09:13 +0600

    init

--- js's/app.js
+++ js's/app.js
@@ -1,12 +1,3 @@
-window.addEventListener('popstate', (event) => {
-      // Redirect to the dashboard
-      window.location.href = "https://210041258.github.io/temp-host/html%27s/dashboard.html";
-    
-  });
-  
-  // Push initial state
-  history.pushState(null, '', location.href);
-  
 
 
 
